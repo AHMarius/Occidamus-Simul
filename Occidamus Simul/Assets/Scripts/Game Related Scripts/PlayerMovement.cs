62a4b9c047ce0ed9648fe477dc9bc0776bc33057
AlMH19 <hritcualex862@gmail.com>
2022-10-28 18:16:24 +0300

    Rotating Weapon
    -Weapon placeholder
    -Weapon rotation towards the mouse

--- Occidamus Simul/Assets/Scripts/Game Related Scripts/PlayerMovement.cs
+++ Occidamus Simul/Assets/Scripts/Game Related Scripts/PlayerMovement.cs
@@ -4,29 +4,29 @@ using Photon.Pun;
 public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed = 5f;
-    private Rigidbody2D rbPlayer;
-    private Vector2 movementInput;
-    private PhotonView view;
+    private Rigidbody2D _rbPlayer;
+    private Vector2 _movementInput;
+    private PhotonView _viewPhoton;
 
     private void Start()
     {
-        rbPlayer = this.GetComponent<Rigidbody2D>();
-        view = GetComponent<PhotonView>();
+        _rbPlayer = this.GetComponent<Rigidbody2D>();
+        _viewPhoton = GetComponent<PhotonView>();
     }
 
     private void Update()
     {
-        if (view.IsMine)
+        if (_viewPhoton.IsMine)
         {
-            movementInput.x = Input.GetAxisRaw("Horizontal");
-            movementInput.y = Input.GetAxisRaw("Vertical");
+            _movementInput.x = Input.GetAxisRaw("Horizontal");
+            _movementInput.y = Input.GetAxisRaw("Vertical");
         }
     }
 
     private void FixedUpdate()
     {
-        rbPlayer.MovePosition(
-            rbPlayer.position + movementInput.normalized * (movementSpeed * Time.fixedDeltaTime)
+        _rbPlayer.MovePosition(
+            _rbPlayer.position + _movementInput.normalized * (movementSpeed * Time.fixedDeltaTime)
         );
     }
 }
